30c9be616b05fe72d0a040ceae25cec29cdf0938
Miguel Valbuena <67212031+Elmigue10@users.noreply.github.com>
2024-04-09 20:13:17 -0500

    Update WebSecurityConfig.java, disable cors

--- information-and-product-management/src/main/java/umb/v1/informationandproductmanagement/application/config/WebSecurityConfig.java
+++ information-and-product-management/src/main/java/umb/v1/informationandproductmanagement/application/config/WebSecurityConfig.java
@@ -10,6 +10,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 import static umb.v1.informationandproductmanagement.domain.utility.Constant.*;
 
 @Configuration
@@ -77,4 +83,15 @@ public class WebSecurityConfig {
                 .build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
 }
